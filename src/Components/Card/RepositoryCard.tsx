707157ad87044f2fbe89857416c7c74ef52f098a
Nick Selivanov <edu.selivanov@yandex.ru>
2024-10-20 08:10:27 +0300

    added a card

---
+++ src/Components/Card/RepositoryCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {IRepository} from "../../type";
+import {Avatar, Card, CardContent, CardHeader, Typography} from "@mui/material";
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import Grid from "@mui/material/Grid2";
+
+import './RepositoryCard.css'
+
+interface ICardProps {
+    repository: IRepository;
+}
+
+const RepositoryCard = ({repository}: ICardProps) => {
+    return (
+        <Grid size={{xs: 6, md: 4, lg: 3}}>
+            <Card className='Card-container'>
+                <CardHeader
+                    avatar={
+                        <Avatar src={repository.owner.avatar_url}/>
+                    }
+                    title={repository.full_name}
+                    subheader={<div className='Card-subheader'>
+                        <StarOutlineIcon sx={{fontSize: '1rem'}}/>
+                        {repository.stargazers_count}
+                    </div>}
+                />
+                <CardContent>
+                    <Typography variant="body2">{repository.description}</Typography>
+                </CardContent>
+            </Card>
+        </Grid>
+    );
+};
+
+export default RepositoryCard;
--- src/Components/CardsList/CardsList.tsx
+++ src/Components/CardsList/CardsList.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Card from "../Card/Card";
+import RepositoryCard from "../Card/RepositoryCard";
 import {searchResult} from "../../type";
+import Grid from "@mui/material/Grid2";
+
 
 interface ICardListProps {
     reposList?: searchResult
@@ -9,10 +11,9 @@ interface ICardListProps {
 const CardsList = ({reposList}:ICardListProps) => {
 
     return (
-        <div>
-            {reposList?.items.map((item) => (<p>{item.stargazers_count}</p>))}
-            <Card/>
-        </div>
+        <Grid container spacing={2} alignItems="stretch">
+            {reposList?.items.map((item) => <RepositoryCard repository={item} />)}
+        </Grid>
     );
 };
 
