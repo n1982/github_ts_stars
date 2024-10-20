85818f37d84809114cf647d805ae05d720cb995c
Nick Selivanov <edu.selivanov@yandex.ru>
2024-10-20 06:50:20 +0300

    getting data from github

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import Search from "./Components/Search/Search";
+import CardsList from "./Components/CardsList/CardsList";
 import './App.css';
+import {searchResult} from "./type";
+
+async function fetchData(): Promise<searchResult> {
+
+    const params = new URLSearchParams({
+        q: "TS",
+        language: "TypeScript",
+        sort: "stars",
+        order: "desc",
+    }).toString();
+    const response = await fetch(
+        `https://api.github.com/search/repositories?${params}`
+    );
+    return response.json();
+}
 
 function App() {
+    const [reposList, setReposList] = React.useState<searchResult | undefined>(undefined)
+
+    useEffect(() => {
+        fetchData().then((response)=>setReposList(response));
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+          <Search/>
       </header>
-        <main></main>
+        <main>
+            <CardsList reposList={reposList} />
+        </main>
     </div>
   );
 }
